19e9116401c27bdcc02fdbcade9f0118cb1b77fc
Buse-Bilici <busebilici21@hotmail.com>
2026-02-15 20:15:16 +0300

    Implemented styled console UI and basic input structure

---
+++ body_mass_index.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <windows.h>
+
+int main() {
+    system("color 2F");  // 0 = siyah arka plan, A = açık yeşil yazı
+
+    printf("🌿Body Mass Index🌿\n"); 
+    
+    float weight;
+     printf("Enter your weight (kg): ");
+    scanf("%f", &weight);
+
+    printf("BMI Calculator\n");
+   
+getchar();
+getchar();
+    
+    return 0;
+}
