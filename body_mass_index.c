22ab09aac80154b7139606173c8389ffa683f12a
Buse-Bilici <busebilici21@hotmail.com>
2026-02-15 20:37:07 +0300

    Create function to calculate Body Mass Index (BMI)

--- body_mass_index.c
+++ body_mass_index.c
@@ -1,6 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
+int bodyMassIndex(int weight , float height)
+{
+    int bmi = weight / (height*height);  // bmi = body mass index
+    return bmi;
+}
+
 int main() {
     system("color 2F");  // 0 = siyah arka plan, A = açık yeşil yazı
 
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,5 @@
+int bodyMassIndex(int weight , float height)
+{
+    int bmi = weight / (height*height);  // bmi = body mass index
+    return bmi;
+}
